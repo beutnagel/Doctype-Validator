2a942f46bf91bb813bf50333404c9a1406c999e0
Jarne W. Beutnagel <jarne@beutnagel.dk>
2016-11-11 17:27:05 +0100

    fixing case sensitivity

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once(__dir__.'/../src/Doctype_Validator.php');
-require_once(__dir__.'/../src/Doctype_Result.php');
+require_once(__dir__.'/../src/doctype_validator.php');
+require_once(__dir__.'/../src/doctype_result.php');
 require_once(__dir__.'/../src/Doctype_Error.php');
 
 
