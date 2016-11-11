ef8e87cd05c99cd94672dde7cf0c03c7f1846472
Jarne W. Beutnagel <jarne@beutnagel.dk>
2016-11-11 21:50:31 +0100

    still trying with codeship

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once(__dir__.'/../vendor/autoload.php');
 require_once(__dir__.'/../src/doctype_validator.php');
 require_once(__dir__.'/../src/doctype_result.php');
 require_once(__dir__.'/../src/Doctype_Error.php');
