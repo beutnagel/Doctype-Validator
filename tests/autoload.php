b445d8b876b65856e50578e4320bad3979d4ce9d
Jarne W. Beutnagel <jarne@beutnagel.dk>
2016-11-11 17:17:02 +0100

    updated autoloader for tests

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once('../src/Doctype_Validator.php');
-require_once('../src/Doctype_Result.php');
-require_once('../src/Doctype_Error.php');
+require_once(__dir__.'/../src/Doctype_Validator.php');
+require_once(__dir__.'/../src/Doctype_Result.php');
+require_once(__dir__.'/../src/Doctype_Error.php');
 
 
