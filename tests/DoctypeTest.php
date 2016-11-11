2c83f30f82fe8bce8567c95e400666b3ce513069
Jarne W. Beutnagel <jarne@beutnagel.dk>
2016-11-11 17:02:41 +0100

    added initial unit tests

---
+++ tests/DoctypeTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Beutnagel;
+use PHPUnit\Framework\TestCase;
+
+class DoctypeTest extends TestCase
+{
+	public function testDoctypeValidatorObjectReturned () {
+		$dt = new Doctype_Validator();
+        $this->assertInstanceOf('Beutnagel\\Doctype_Validator', $dt);
+	}
+	public function testDoctypeResultObjectReturned () {
+		$dt = new Doctype_Result();
+        $this->assertInstanceOf('Beutnagel\\Doctype_Result', $dt);
+	}
+
+	public function testDoctypeResultObjectReturnedFromValidator () {
+		$dt = new Doctype_Validator();
+		$result = $dt->validate("<!DOCTYPE html>");
+        $this->assertInstanceOf('Beutnagel\\Doctype_Result', $result);
+	}
+
+
+}
