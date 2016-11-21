ed54f90bb2278c9a0968e91e36aa2ccbe7689291
Jarne W. Beutnagel <jarne@beutnagel.dk>
2016-11-21 22:43:27 +0100

    bootstrap path issues

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,14 @@
 <?php
 
-require_once(realpath(__DIR__ . '/..').'/vendor/autoload.php');
-require_once(realpath(__DIR__ . '/..').'/src/doctype_validator.php');
-require_once(realpath(__DIR__ . '/..').'/src/doctype_result.php');
-require_once(realpath(__DIR__ . '/..').'/src/Doctype_Error.php');
+$root    = realpath(dirname(dirname(__FILE__)));
+
+if (!file_exists($root . '/vendor/autoload.php')) {
+    throw new Exception(
+        'Please run "php composer.phar install --dev" in root directory '
+        . 'to setup unit test dependencies before running the tests'
+    );
+}
+require_once(realpath($root).'/vendor/autoload.php');
+require_once(realpath($root).'/src/doctype_validator.php');
+require_once(realpath($root).'/src/doctype_result.php');
+require_once(realpath($root).'/src/Doctype_Error.php');
