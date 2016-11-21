1d92614da40326f4c3a928891857786edde27844
Jarne W. Beutnagel <jarne@beutnagel.dk>
2016-11-21 21:57:50 +0100

    updated phpunit bootstrap path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('./vendor/autoload.php');
-require_once('./src/doctype_validator.php');
-require_once('./src/doctype_result.php');
-require_once('./src/Doctype_Error.php');
+require_once(realpath(__DIR__ . '/..').'/vendor/autoload.php');
+require_once(realpath(__DIR__ . '/..').'/src/doctype_validator.php');
+require_once(realpath(__DIR__ . '/..').'/src/doctype_result.php');
+require_once(realpath(__DIR__ . '/..').'/src/Doctype_Error.php');
